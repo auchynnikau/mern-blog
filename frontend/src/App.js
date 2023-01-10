7c087173c019cf86918fa9df1fa7bf67bfc19217
Viachaslau Auchynnikau <viachaslau_auchynnikau@epam.com>
2023-01-10 22:33:25 +0300

    feat: user auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import Container from "@mui/material/Container";
+
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import React from "react";
+import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
 
 function App() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, []);
+
   return (
     <>
       <Header />
